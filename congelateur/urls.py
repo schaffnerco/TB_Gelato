73c347d385b970e8d1c5485d03812187831db46e
schaffnerco <colin.schaffner@he-arc.ch>
2016-03-31 07:33:25 +0200

    Modifications des urls

--- congelateur/urls.py
+++ congelateur/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = [
-    url(r'^accueil$', views.accueil),
-    url(r'^congelateur$', views.congelateur),
-    url(r'^home$', views.home),
-    url(r'^produits$', views.produits),
-    url(r'^about$', views.about)
+    url(r'^accueil$', views.accueil, name='accueil'),
+    url(r'^congelateur$', views.congelateur, name='congo'),
+    url(r'^home$', views.home, name='home'),
+    url(r'^produits$', views.produits, name='produit'),
+    url(r'^about$', views.about, name='about')
 
 ]
 
