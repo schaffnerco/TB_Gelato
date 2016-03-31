ae70151761a894fdf5d32620eac97052424abf25
schaffnerco <colin.schaffner@he-arc.ch>
2016-03-31 07:20:07 +0200

    Début du 31.03.2016

--- congelateur/views.py
+++ congelateur/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from congelateur.models import Glace
+from congelateur.models import Glace, Congelateur
 
 from django.views.generic import ListView
 
@@ -22,5 +22,9 @@ def produits(request):
     listeGlaces = Glace.objects.all()
     #return render(request, 'products.html')
     return render(request, 'products.html', {'glaces': listeGlaces})
+
+def congelateur(request):
+    listeCongos = Congelateur.objects.all()
+    return render(request, 'congelateur.html', {'congos':listeCongos})
 
 
